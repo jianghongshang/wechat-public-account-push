62be6723f29782c6a41233fd499c0cbe1318eea8
dreamjhs <61773739+jianghongshang@users.noreply.github.com>
2023-05-17 17:06:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '晓梦宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oamdA5p-buQzvWA-oz76R7JOzjGo',
+      id: 'oamdA5j_snTNUv_4Bnwfmfm8z7uQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oamdA5j_snTNUv_4Bnwfmfm8z7uQ',
+      useTemplateId: 'GVeLv1AGL1QD-eEEYvAXM8YEnGOdSPvkzO_XEt8-dNw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-21',
       festivals: [
