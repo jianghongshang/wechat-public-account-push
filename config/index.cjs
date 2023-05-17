fb65cb8a2135db3217e338adb9c3acba3bd16892
dreamjhs <61773739+jianghongshang@users.noreply.github.com>
2023-05-17 16:32:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2023-04-30' },
+        { keyword: 'love_day', date: '2023-04-30' },
       ],
     },
   ],
