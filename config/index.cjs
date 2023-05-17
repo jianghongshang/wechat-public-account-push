6816cb52ce4157f0af91c964cf035e81ccf878a0
dreamjhs <61773739+jianghongshang@users.noreply.github.com>
2023-05-17 16:55:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oamdA5p-buQzvWA-oz76R7JOzjGo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GVeLv1AGL1QD-eEEYvAXM8YEnGOdSPvkzO_XEt8-dNw',
+      useTemplateId: 'oamdA5j_snTNUv_4Bnwfmfm8z7uQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-21',
       festivals: [
